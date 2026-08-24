f52e9b106bbc966d97c5e58cc5156a82e7e0c636
ChanChoronaiy <a1978787@adelaide.edu.au>
2026-08-24 17:13:10 +0930

    chsnging test file name

---
+++ Assignment1/src/test/java/cloud/concurrent/assignment1/Assignment1ApplicationTests.java
@@ -0,0 +1,14 @@
+package cloud.concurrent.assignment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/* Basic regression test proving that Spring can create the complete application  */
+@SpringBootTest
+class Assignment1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // If a dependency is missing or incorrectly configured, Spring fails before this returns
+    }
+}
